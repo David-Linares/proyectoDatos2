a87b0ea3239a832dd2459517464ba72e6d369b9d
Daniel501 <danielbel1994@hotmail.com>
2014-11-10 19:04:55 -0500

    cambios

--- src/controlador/ConexionServidor.java
+++ src/controlador/ConexionServidor.java
@@ -6,6 +6,7 @@ import java.net.Socket;
 
 import vista.Principal;
 import vista.VendedorSubasta;
+
 import javax.swing.JOptionPane;
 
 public class ConexionServidor extends Thread {
@@ -21,29 +22,30 @@ public class ConexionServidor extends Thread {
 	
 	//OK
 	@SuppressWarnings("resource")
-	public synchronized void run() {
+	public void run() {
 		ServerSocket sServidor = null;
 		ConexionClienteServidor nuevaConexion = null;
 		try {
 			sServidor = new ServerSocket(puerto);
 			while (true) {
-				try {
+				
 					Socket nuevoSServidor = sServidor.accept();
-					System.out.println("CServidor / Se conectó un nuevo cliente ");
-					System.out.println("CServidor / variable Conexion TEMP "+General.getConexionTemp());
 					nuevaConexion = new ConexionClienteServidor(nuevoSServidor);					
 					General.setConexionTemp(nuevaConexion);
-				} catch (IOException e) {
-					JOptionPane.showMessageDialog(ventana, "CServidor / Se produjo un error en la nueva conexión");
-				}
+				
 			}
 		} catch (IOException e1) {
 			JOptionPane.showMessageDialog(ventana, "Ya hay una subasta abierta. \n Por Favor ingresa como cliente.", "Error en creaci\u00f3n de Subasta", JOptionPane.INFORMATION_MESSAGE, general.getIcon("error"));
 			Principal ventanaPrincipal = new Principal();
 			ventanaPrincipal.getBtnNuevaSubasta().setEnabled(false);
 			ventanaPrincipal.setVisible(true);
-			return;
-		}
+		try {
+            sServidor.close();
+        }catch(IOException e2){
+         JOptionPane.showMessageDialog(ventana, "CServidor / no se cerr� el puerto + e2.getMessage()");	
+        }
+	
 	}
-
+	
+	}
 }
